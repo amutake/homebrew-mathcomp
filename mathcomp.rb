98b567497827c0fcbbd1d5c32d32e363ab527819
amutake <amutake.s@gmail.com>
2014-09-07 13:51:58 +0900

    switch md5 to sha1

--- mathcomp.rb
+++ mathcomp.rb
@@ -4,7 +4,7 @@ class Mathcomp < Formula
 
   homepage "http://ssr.msr-inria.inria.fr/"
   url "http://ssr.msr-inria.inria.fr/FTP/mathcomp-1.5.tar.gz"
-  md5 "f6f24df228569f0095d710063a633709"
+  sha1 "4601d61092fc3ebd7279c7c3efb53de5ec2c9912"
   version "1.5"
   depends_on "ssreflect"
 
